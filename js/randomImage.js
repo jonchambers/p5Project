c3cff9576a7849ac2e0848636edd1b8e31aaeecc
student <student@ecp-c-8814-cp.attlocal.net>
2020-10-26 15:14:13 -0500

    additional notes to randomImage.js

--- js/randomImage.js
+++ js/randomImage.js
@@ -7,6 +7,8 @@ function preload() {
     //adding images to the empty imgs array
     //in this case, the images need to be named 0.png, 1.png, 2.png, etc
     //and be indside the images folder
+    //You also need to change the for loop number to match how many images you have...so it's 2 right now b/c i have two image
+    //if you have 10 images, it should be i<10
     imgs[i] = loadImage("images/" + i + ".png");
   }
 }
