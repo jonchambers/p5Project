99b040a822fe1e09360e48bcc4792493513c6fec
student <student@ecp-c-8814-cp.attlocal.net>
2020-10-26 17:21:41 -0500

    added booleans

--- js/simpleCheckBox.js
+++ js/simpleCheckBox.js
@@ -5,6 +5,7 @@ let check1;
 let check2;
 let check3;
 
+let drawBool = false;
 
 let button;
 
@@ -28,40 +29,56 @@ function setup() {
   check3.style('position', 'relative');
 
   button = createButton("submit");
-  button.mousePressed(checkFunction);
   button.style('position', 'relative');
 
-  // check1.changed(checkFunction);
-  // check2.changed(checkFunction);
-  // check3.changed(checkFunction);
+  //trigger function when button is pressed
+  button.mousePressed(checkFunction);
+
 
 }
 
+//check the status of the checkboxes and change the background
+//depending on what checkbox is checked.
+//you can also flip booleans here too, to trigger an function that
+//needs to be updated like in check3 below.
 function checkFunction(){
   if(check1.checked()){
 
-    // check2.checked(false);
-    // check3.checked(false);
     background(255, 0, 0);
+    drawBool = false;
   }
 
   if(check2.checked()){
 
-        background(0, 255, 0);
+    background(0, 255, 0);
+    drawBool = false;
   }
 
   if(check3.checked()){
-      background(0, 0, 255);
+    background(0, 0, 255);
+    drawBool = true;
+
   }
 
-   check1.checked(false);
-    check2.checked(false);
-     check3.checked(false);
+  //reset the checkboxes to unchecked
+  check1.checked(false);
+  check2.checked(false);
+  check3.checked(false);
 
+}
+
+function pencilFunction(){
+  strokeWeight(10);
+  stroke(255);
+  line(pmouseX, pmouseY, mouseX, mouseY);
 }
 
 
 function draw(){
 
+  //if drawBool is true, calll the pencil function
+  if(drawBool == true){
+    pencilFunction();
+  }
 
 }
